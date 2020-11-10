f7c220028f859ed3c62af3c45aefed6d7cf09623
Harshit Singhai <harshitsinghai77@gmail.com>
2020-11-11 00:31:21 +0530

    hey, my new post. Done for today

--- script.sh
+++ script.sh
@@ -1,8 +1,7 @@
 git checkout -b new-post
 git add .
 git commit -m "hey, my new post. Done for today"
-git push
-
+git push --set-upstream origin new-post
 git branch -d new-post
 git push origin --delete new-post
 
