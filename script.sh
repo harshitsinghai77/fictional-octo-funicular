1368368c5233b0c4aff21caa288ca041bd7082d1
root <root@pop-os.localdomain>
2020-11-11 00:32:09 +0530

    hey, my new post. Done for today

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@ git checkout -b new-post
 git add .
 git commit -m "hey, my new post. Done for today"
 git push --set-upstream origin new-post
-git branch -d new-post
-git push origin --delete new-post
+# git branch -d new-post
+# git push origin --delete new-post
 
