3d8658e561141a1cfc7141056a271963b4504cc4
harshitsinghai77 <harshitsinghai77@gmail.com>
2021-07-16 20:06:24 +0530

    fix passing issue

--- components/container.js
+++ components/container.js
@@ -1,6 +1,6 @@
 export default function Container({ children }) {
   return (
-    <div className="mx-auto px-5 sm:container xl:px-20 md:container lg:container xl:container 2xl:container">
+    <div className="mx-auto px-5 2xl:px-20 sm:container xl:px-20 md:container lg:container xl:container 2xl:container">
       {children}
     </div>
   );
