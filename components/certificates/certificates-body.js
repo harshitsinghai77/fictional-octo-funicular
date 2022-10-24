7faabca7e95a56446be9889f6ed40272f83ed94b
harshitsinghai77 <harshitsinghai77@gmail.com>
2022-10-24 10:31:59 +0530

    Add conditional certificate width value based on uselayouteffect width

--- components/certificates/certificates-body.js
+++ components/certificates/certificates-body.js
@@ -1,25 +1,47 @@
+import { useLayoutEffect, useRef, useState, memo } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
 
 const rootCertificateDirectory = "certificate/";
 
-export default function CertificateCard({ certificates }) {
+const CertificateCard = ({ certificates }) => {
+  const ref = useRef(null);
+  const [documentWidth, setDocumentWidth] = useState(550);
+
+  useLayoutEffect(() => {
+    console.log("ref.current.offsetWidth", ref.current.offsetWidth);
+    let width = 550;
+    if (ref.current.offsetWidth < 450) {
+      width = 420;
+    }
+    if (ref.current.offsetWidth < 400) {
+      width = 320;
+    }
+    setDocumentWidth(width);
+  }, []);
+
   const RenderCertificate = () =>
     certificates.map((certificate) => {
       const filePath = rootCertificateDirectory + certificate;
       return (
         <div>
           <Document file={filePath} loading="Loading Certificate">
-            <Page pageNumber={1} width={550} />
+            <Page pageNumber={1} width={documentWidth} />
           </Document>
         </div>
       );
     });
 
   return (
-    <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2">
+    <div
+      div
+      ref={ref}
+      className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2"
+    >
       <RenderCertificate />
     </div>
   );
-}
+};
+
+export default memo(CertificateCard);
